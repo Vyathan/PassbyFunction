01e8f4b6a2f39c4869a3eff4fbb76b4c25562cdf
Matt Brundage II <mattjbrundage@icloud.com>
2021-04-18 12:41:23 -0400

    First Code

--- PassbyFunction.cpp
+++ PassbyFunction.cpp
@@ -2,3 +2,37 @@
 // Date: 4/18/21
 // Filename: PassbyFunction.cpp
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double totalCst, qty, price, dscnt;
+
+    //Request User Input
+    cout << "Please Enter Product Price: ";
+    cin >> price;
+    cout << "Please Enter Product Quantity: ";
+    cin >> qty;
+
+    //Calculate Result
+    totalCst = price * qty;
+
+    //Calculate Discount
+    if(totalCst > 100.00)
+    {
+        dscnt = (totalCst * .05);
+        totalCst = totalCst - dscnt;
+    }
+
+    //Display Result
+    cout << "The total cost of this product is: $" << totalCst << " and the individual cost is: $" << price << endl;
+
+    return 0;
+
+    //Display complete message
+    cout << "Cost Program Completed." << endl;
+    
+}   //End Program
